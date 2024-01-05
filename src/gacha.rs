8a07a0c4fd323efa8faa8b663a7d98f41cbb1a32
0num4 <r.oonuma@matsuri-tech.com>
2024-01-06 04:01:54 +0900

    feat: 引数を追加

--- src/gacha.rs
+++ src/gacha.rs
@@ -1,12 +1,12 @@
 pub fn gacha() {
     println!("ガチャガチャ");
-    kakuritu()
+    kakuritu(20.0)
 }
 
-pub fn kakuritu() {
+pub fn kakuritu(count: f64) {
     // let atari_kakuritu = 0.05; // 5%
     let hazure: f64 = 0.95; // 95%
-    let kakuritu = hazure.powf(10.0);
+    let kakuritu = hazure.powf(count);
     print!("10連引いて外れる確率は{:.1}%です。", (kakuritu * 100.0));
     println!("確率");
 }
