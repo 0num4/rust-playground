adc97b12abb88882366a0607ef8b2cd73e4cd271
0num4 <r.oonuma@matsuri-tech.com>
2024-01-06 03:59:19 +0900

    feat: 10連引いて当たる確率

--- src/gacha.rs
+++ src/gacha.rs
@@ -1,3 +1,12 @@
 pub fn gacha() {
     println!("ガチャガチャ");
+    kakuritu()
+}
+
+pub fn kakuritu() {
+    // let atari_kakuritu = 0.05; // 5%
+    let hazure: f64 = 0.95; // 95%
+    let kakuritu = hazure.powf(10.0);
+    print!("10連引いて外れる確率は{:.1}%です。", (kakuritu * 100.0));
+    println!("確率");
 }
