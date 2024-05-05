2c1ea60cf53e4ce20fa901ce52805c71e86336d7
0num4 <r.oonuma@matsuri-tech.com>
2024-05-05 10:14:24 +0900

    feat: forに修正

--- src/q3.rs
+++ src/q3.rs
@@ -4,14 +4,12 @@ pub fn main() {
     let s = "This is a pen. That is a pencil.";
     let v = s.chars().collect::<Vec<char>>();
     let mut hashmap: HashMap<char, i32> = HashMap::new();
-    v.iter()
-        .map(|x| {
-            let mut prev = hashmap.get(x).unwrap_or(&0);
-            if (*x).is_ascii_alphabetic() {
-                hashmap.insert(*x, *prev + 1);
-            }
-        })
-        .collect::<Vec<_>>();
+    for c in v {
+        if c.is_alphabetic() {
+            let prev = hashmap.get(&c).unwrap_or(&0);
+            hashmap.insert(c, prev + 1);
+        }
+    }
     let max = hashmap.iter().max_by_key(|x| x.1).unwrap();
     println!("hashmap {:?}", hashmap);
     println!("値が一番大きいもの: {:?}", max.0)
