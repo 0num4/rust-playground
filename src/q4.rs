0c70041a65c50f56702a8c1702f537c01415b1ad
0num4 <r.oonuma@matsuri-tech.com>
2024-05-06 22:53:12 +0900

    feat: コンパイル通った2

--- src/q4.rs
+++ src/q4.rs
@@ -22,37 +22,37 @@ pub fn main() {
     let arcMutexTest = Arc::new(Mutex::new(43));
     let mut arcMutexTestInner = arcMutexTest.lock().unwrap();
     println!("{}", arcTest);
-    println!("{}", arcMutexTest.lock().unwrap());
+    println!("{}", arcMutexTestInner);
     // *arcTest = 444;
     *arcMutexTestInner = 444;
     println!("{}", arcTest);
     println!("{}", arcMutexTestInner);
-    // q4_pre();
+    q4_pre();
 }
 
-// fn q4_pre() {
-//     let shared_int = Arc::new(Mutex::new(32));
-//     let mut thread_dequeue = VecDeque::new();
-//     for _ in 1..10 {
-//         let t = std::thread::spawn(move || {
-//             let clone_shared_int = Arc::clone(&shared_int);
-//             let mut locked_shared_int = clone_shared_int.lock().unwrap();
-//             println!("{}", locked_shared_int);
-//             for _ in 1..1000 {
-//                 *locked_shared_int = *locked_shared_int + 1;
-//             }
-//             println!("locked_shared_int: {}", *locked_shared_int);
-//             println!("5s待ちます");
-//             std::thread::sleep(std::time::Duration::from_secs(5));
-//             println!("5s待ちました");
-//         });
-//         thread_dequeue.push_back(t);
-//         // let t_res = t.await;
-//         // println!("{:?}", t_res)
-//     }
+fn q4_pre() {
+    let shared_int = Arc::new(Mutex::new(32));
+    let mut thread_dequeue = VecDeque::new();
+    for _ in 1..10 {
+        let clone_shared_int = Arc::clone(&shared_int);
+        let t = std::thread::spawn(move || {
+            let mut locked_shared_int = clone_shared_int.lock().unwrap();
+            println!("{}", locked_shared_int);
+            for _ in 1..1000 {
+                *locked_shared_int = *locked_shared_int + 1;
+            }
+            println!("locked_shared_int: {}", *locked_shared_int);
+            println!("5s待ちます");
+            std::thread::sleep(std::time::Duration::from_secs(5));
+            println!("5s待ちました");
+        });
+        thread_dequeue.push_back(t);
+        // let t_res = t.await;
+        // println!("{:?}", t_res)
+    }
 
-//     while thread_dequeue.len() == 0 {
-//         let t = thread_dequeue.pop_back();
-//         t.unwrap().join();
-//     }
-// }
+    while thread_dequeue.len() == 0 {
+        let t = thread_dequeue.pop_back();
+        t.unwrap().join();
+    }
+}
