ab9acc829490d24b32b9f7e21c52fc8e837d118f
0num4 <r.oonuma@matsuri-tech.com>
2024-05-08 14:57:07 +0900

    feat: jsonとして処理するメソッドお持つ以下

--- src/read_json.rs
+++ src/read_json.rs
@@ -1,5 +1,7 @@
 use std::fs::File;
 
+use serde_json::{value, Value};
+
 // {
 //     "$schema": "https://docs.renovatebot.com/renovate-schema.json",
 //     "extends": [
@@ -16,7 +18,10 @@ struct Renovate {
 
 pub fn main() {
     let mut f = File::open("renovate.json").expect("ファイルが開けませんでした");
+    let jsonValue: Value = serde_json::from_reader(f).expect("jsonのパースに失敗しました");
+    println!("json value is: {:?}", jsonValue);
 
-    let mut json: Renovate = serde_json::from_reader(f).expect("jsonのパースに失敗しました");
-    println!("{:?}", json);
+    // 構造体に代入
+    // let mut json: Renovate = serde_json::from_reader(f).expect("jsonのパースに失敗しました");
+    // println!("{:?}", json);
 }
