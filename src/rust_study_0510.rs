5cbc0e91b74873a566b6aa24403254ae8382a6eb
0num4 <r.oonuma@matsuri-tech.com>
2024-05-10 23:35:03 +0900

    feat: add sample

--- src/rust_study_0510.rs
+++ src/rust_study_0510.rs
@@ -1,21 +1,25 @@
+use chrono::prelude;
 use std::{default, fs::File, io::Read};
 
 pub fn main() {
-    let mut s: String = String::new();
-    let mut f = File::open("./src/rust_study_0510.log").expect("no such file or dirs");
-    f.read_to_string(&mut s).unwrap();
-    // println!("{}", s);
-    #[derive(Default)]
-    struct Ers {
-        info_count: i32,
-        warn_count: i32,
-        err_count: i32,
-    }
-    let mut ers: Ers = Ers::default();
-    for l in s.lines() {
-        let lsp: std::str::Split<&str> = l.split(",");
-        assert_eq!(lsp.count(), 3);
-    }
+    errsample1();
+    oksample1();
+    //     let mut s: String = String::new();
+    //     let mut f = File::open("./src/rust_study_0510.log").expect("no such file or dirs");
+    //     f.read_to_string(&mut s).unwrap();
+    //     // println!("{}", s);
+    //     #[derive(Default)]
+    //     struct Ers {
+    //         info_count: i32,
+    //         warn_count: i32,
+    //         err_count: i32,
+    //     }
+    //     let mut ers: Ers = Ers::default();
+    //     for l in s.lines() {
+    //         let mut lsp: std::str::Split<&str> = l.split(",");
+    //         assert_eq!(lsp.count(), 3);
+    //         let lsp2 = lsp.next().unwrap();
+    //     }
 }
 
 // #[cfg(test)]
@@ -25,3 +29,17 @@ pub fn main() {
 
 //     }
 // }
+
+pub fn errsample1() {
+    // 参照エラーになるサンプル
+    let x = String::new();
+    let y = x;
+    println!("x is {:?}", x);
+}
+
+pub fn oksample1() {
+    // これは変数がスタック上に乗ってるのでok
+    let x = 5;
+    let y = x;
+    println!("x is {:?}", x);
+}
