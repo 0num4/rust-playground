a89195f6fae44c4f9c918b90f5eaf881ecc8d61a
0num4 <r.oonuma@matsuri-tech.com>
2024-08-13 05:41:27 +0900

    feat: Dependenciesを外すとうまく動く

--- src/cargo_toml_parse.rs
+++ src/cargo_toml_parse.rs
@@ -22,7 +22,7 @@ struct Dependencies {
 #[derive(Deserialize)]
 struct Cargo {
     package: Package,
-    dependencies: Dependencies,
+    // dependencies: Dependencies,
 }
 
 pub fn main() {
@@ -30,3 +30,9 @@ pub fn main() {
     let cargo_toml: Cargo = toml::from_str(&cargotomlstring).unwrap();
     println!("cargo_toml.package.name {:?}", cargo_toml.package.name);
 }
+
+// ```
+// thread 'main' panicked at src/cargo_toml_parse.rs:30:62:
+// called `Result::unwrap()` on an `Err` value: Error { inner: Error { inner: TomlError { message: "missing field `version`", raw: Some("[package]\nname = \"rust-playground\"\nversion = \"0.1.0\"\nedition = \"2021\"\nlicense = \"MIT\"\ndescription = \"A simple Rust playground\"\n# See more keys and their definitions at https://doc.rust-lang.org/cargo/reference/manifest.html\n\n[dependencies]\nanyhow = \"1.0.86\"\ncargo-insta = \"1.39.0\"\nchrono = \"0.4.38\"\ndiesel = { version = \"2.2.1\", features = [\"postgres\"] }\ndotenvy = \"0.15.7\"\nhttp-body-util = \"0.1.1\"\nhyper = { version = \"1.3.1\", features = [\"full\"] }\nhyper-util = { version = \"0.1.3\", features = [\"full\"] }\nlog = \"0.4.21\"\nproconio = \"0.4.5\"\nrand = \"0.8.5\"\nserde = { version = \"1.0.200\", features = [\"derive\"] }\nserde_json = \"1.0.116\"\ntokio = { version = \"1.37.0\", features = [\"full\"] }\ntoml = \"0.8.19\"\n"), keys: ["dependencies"], span: Some(225..700) } } }
+// note: run with `RUST_BACKTRACE=1` environment variable to display a backtrace
+// ```
