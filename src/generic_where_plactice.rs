6fb92c61a44ee31472a01dd761d596b6ca9813a3
0num4 <r.oonuma@matsuri-tech.com>
2024-08-13 08:23:43 +0900

    feat: impl Trait for Structなどで i32とgenericsは混合することができる

--- src/generic_where_plactice.rs
+++ src/generic_where_plactice.rs
@@ -1,7 +1,7 @@
 pub fn main() {
     let n = N { s: 4 };
-    let s = genericsample(n);
-    println!("{:?}", s)
+    // let s = genericsample(n);
+    // println!("{:?}", s)
 }
 trait SS<T> {
     fn ss(&self) -> i32;
@@ -10,7 +10,7 @@ struct N<T> {
     s: T,
 }
 
-impl<T> SS<T> for N<T> {
+impl<T> SS<T> for N<i32> {
     fn ss(&self) -> i32 {
         return self.s * self.s;
     }
