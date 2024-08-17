d9b861aeeebc545bce3b93b3bde22c448fbe8386
0num4 <r.oonuma@matsuri-tech.com>
2024-08-18 00:40:39 +0900

    feat: abc129.rs

---
+++ src/abc129.rs
@@ -0,0 +1,17 @@
+use proconio::input;
+pub fn main() {
+    // a();
+}
+
+fn a() {
+    input! {}
+}
+
+// 累積和の練習
+// https://atcoder.jp/contests/abc129/tasks/abc129_b
+fn b() {
+    input! {
+        n: i32,
+        w: [i32; n]
+    }
+}
